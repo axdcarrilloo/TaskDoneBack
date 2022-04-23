2f9b99d2c3bc26cb259969e96624e0147aaed0ca
axdcarrilloo <63140558+axdcarrilloo@users.noreply.github.com>
2022-04-23 10:51:21 -0500

    1er commit

---
+++ src/main/java/com/TaskDoneBack/TaskDoneBackApplication.java
@@ -0,0 +1,13 @@
+package com.TaskDoneBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskDoneBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskDoneBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TaskDoneBack/TaskDoneBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TaskDoneBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskDoneBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
